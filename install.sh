a2a2680d35e5417e607a28aa826c076242f45054
tsuchigumoo <35763044+tsuchigumoo@users.noreply.github.com>
2019-06-07 11:39:35 +0200

    Add files via upload

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ apt-get install ranger -y
 apt-get install scrot -y
 apt-get install screenfetch -y
 apt-get install feh -y
+apt-get install xsel -y
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp /home/install/vimrc /etc/vim/vimrc
 cp /home/install/bashrc .bashrc
