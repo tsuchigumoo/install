aeac5e9194cd4631a05135ca6c463abffd2c7743
tsuchigumoo <35763044+tsuchigumoo@users.noreply.github.com>
2019-06-12 14:33:10 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,6 +7,11 @@ apt-get install scrot -y
 apt-get install screenfetch -y
 apt-get install feh -y
 apt-get install xsel -y
+apt-get install konsole -y
+apt-get install dmenu -y
+apt-get install i3
+apt-get install i3status
+
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp /home/install/vimrc /etc/vim/vimrc
 cp /home/install/bashrc .bashrc
